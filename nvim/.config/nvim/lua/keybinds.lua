22bd2bd33c3bebd3f6361cabcc4789198fc89bd6
Czékus Máté <mate@picloud.hu>
2025-01-05 13:50:12 +0100

    fix: nvim telescope file browser
    Signed-off-by: Czékus Máté <mate@picloud.hu>

--- nvim/.config/nvim/lua/keybinds.lua
+++ nvim/.config/nvim/lua/keybinds.lua
@@ -1,29 +1,29 @@
 require("functions.git")
 require("functions.copilot")
 
-vim.api.nvim_set_keymap("n", "<leader>fs", ":Telescope grep_string<CR>", { noremap = true, desc = "[f]ind [s]tring" })
-vim.api.nvim_set_keymap("n", "<leader>fa", ":Telescope live_grep<CR>", { noremap = true, desc = "[f]ind [s]tring" })
-vim.api.nvim_set_keymap("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true, desc = "[f]ind [f]iles" })
-vim.api.nvim_set_keymap("n", "<leader>fb", ":Telescope file_browser<CR>", { noremap = true, desc = "[f]ind [b]rowser" })
+vim.api.nvim_set_keymap("n", "<leader>fs", ":Telescope grep_string<CR>", { noremap = true, silent = true, desc = "[f]ind [s]tring" })
+vim.api.nvim_set_keymap("n", "<leader>fa", ":Telescope live_grep<CR>", { noremap = true, silent = true, desc = "[f]ind [s]tring" })
+vim.api.nvim_set_keymap("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true, silent = true, desc = "[f]ind [f]iles" })
+vim.api.nvim_set_keymap("n", "<leader>fb", ":Telescope file_browser<CR>", { noremap = true, silent = true, desc = "[f]ind [b]rowser" })
 
-vim.api.nvim_set_keymap("n", "<leader>Gcs", ":Telescope git_commits<CR>", { noremap = true, desc = "[G]it [c]ommits" })
-vim.api.nvim_set_keymap("n", "<leader>Gb", ":Telescope git_branches<CR>", { noremap = true, desc = "[G]it [b]ranches" })
-vim.keymap.set("n", "<leader>Gp", GitPush, { noremap = true, desc = "[G]it [p]ush" })
+vim.api.nvim_set_keymap("n", "<leader>Gcs", ":Telescope git_commits<CR>", { noremap = true, silent = true, desc = "[G]it [c]ommits" })
+vim.api.nvim_set_keymap("n", "<leader>Gb", ":Telescope git_branches<CR>", { noremap = true, silent = true, desc = "[G]it [b]ranches" })
+vim.keymap.set("n", "<leader>Gp", GitPush, { noremap = true, silent = true, desc = "[G]it [p]ush" })
 vim.api.nvim_set_keymap(
   "n",
   "<leader>Gh",
   ":Gitsigns toggle_current_line_blame<CR>",
-  { noremap = true, desc = "[G]it [h]istory" }
+  { noremap = true, silent = true, desc = "[G]it [h]istory" }
 )
 vim.keymap.set("n", "<leader>Gc", GitCommit, { noremap = true, silent = true, desc = "[g]it [c]ommit" })
 
-vim.api.nvim_set_keymap("n", "<C-f>", ":Telescope current_buffer_fuzzy_find<CR>", { noremap = true, desc = "[F]ind" })
+vim.api.nvim_set_keymap("n", "<C-f>", ":Telescope current_buffer_fuzzy_find<CR>", { noremap = true, silent = true, desc = "[F]ind" })
 
-vim.api.nvim_set_keymap("n", "<leader>tt", ":sp term://bash<CR>", { noremap = true, desc = "[tt]erminal" })
+vim.api.nvim_set_keymap("n", "<leader>tt", ":sp term://bash<CR>", { noremap = true, silent = true, desc = "[tt]erminal" })
 
-vim.api.nvim_set_keymap("n", "<leader>sk", ":Telescope keymaps<CR>", { noremap = true, desc = "[s]earch [k]eymaps" })
+vim.api.nvim_set_keymap("n", "<leader>sk", ":Telescope keymaps<CR>", { noremap = true, silent = true, desc = "[s]earch [k]eymaps" })
 
-vim.api.nvim_set_keymap("n", "<leader><TAB>", ":Telescope buffers<CR>", { noremap = true, desc = "show open buffers" })
+vim.api.nvim_set_keymap("n", "<leader><TAB>", ":Telescope buffers<CR>", { noremap = true, silent = true, desc = "show open buffers" })
 
 vim.keymap.set("n", "<C-S-Up>", ":m -2<CR>", { noremap = true, desc = "move line up", silent = true })
 vim.keymap.set("n", "<C-S-Down>", ":m +1<CR>", { noremap = true, desc = "move line down", silent = true })
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,10 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.6",
+    tag = "0.1.8",
     config = function()
-      local telescopeConfig = require("telescope.config")
-      local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+      local telescope_config = require("telescope.config")
+      local vimgrep_arguments = { unpack(telescope_config.values.vimgrep_arguments) }
       -- Add search option for hidden files except .git
       table.insert(vimgrep_arguments, "--hidden")
       table.insert(vimgrep_arguments, "--glob")
@@ -12,6 +12,12 @@ return {
       table.insert(vimgrep_arguments, "--glob")
       table.insert(vimgrep_arguments, "!**/node_modules/**")
       require("telescope").setup({
+        extensions = {
+          file_browser = {
+            follow_symlinks = true,
+            hidden = true,
+          },
+        },
         defaults = {
           vimgrep_arguments = vimgrep_arguments,
         },

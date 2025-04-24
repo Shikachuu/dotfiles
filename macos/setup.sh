b6222d21ccfa0d922deb5dc74c352d09b2d640b0
Máté Czékus <mate@picloud.hu>
2025-04-24 07:40:58 +0200

    feat: add rectangle shortcuts
    Signed-off-by: Máté Czékus <mate@picloud.hu>

--- macos/setup.sh
+++ macos/setup.sh
@@ -15,6 +15,26 @@ defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
+# Disable hotkeys that captures the ctrl + space shortcut
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 60 "{ enabled = 0; }"
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 61 "{ enabled = 0; }"
+defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 62 "{ enabled = 0; }"
+defaults write com.apple.WindowManager GloballyEnabled -bool true
+
+# Bind fill window to Control+Option+Enter
+defaults write NSGlobalDomain NSUserKeyEquivalents -dict-add "\033Window\033Fill" "^~\\U21a9"
+# Bind center window to Control+Option+c
+defaults write NSGlobalDomain NSUserKeyEquivalents -dict-add "\033Window\033Center" "^~c"
+# Bind arranging window left side to Control+Option+Left Arrow
+defaults write NSGlobalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Left" "~^\\U2190"
+# Bind arranging window right side to Control+Option+Right Arrow
+defaults write NSGlobalDomain NSUserKeyEquivalents -dict-add "\033Window\033Move & Resize\033Right" "~^\\U2192"
+
+# WindowManager settings
+defaults write com.apple.WindowManager EnableTiledWindowMargins -bool false
+defaults write com.apple.WindowManager EnableTilingByEdgeDrag -bool true
+defaults write com.apple.WindowManager EnableTilingOptionAccelerator -bool true
+
 brew_bash_path=$(ls "$(brew --prefix)/bin/bash")
 
 if ! grep -Fxq "$brew_bash_path" /etc/shells; then

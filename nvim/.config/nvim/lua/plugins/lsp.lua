fd9b19d2d9f6aebff660a755fa8cfdf706c79225
Máté Czékus <mate@picloud.hu>
2026-06-29 09:06:37 +0200

    feat: add flutter tools
    Signed-off-by: Máté Czékus <mate@picloud.hu>

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -96,4 +96,13 @@ return {
       },
     },
   },
+  {
+    "nvim-flutter/flutter-tools.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim",
+    },
+    config = true,
+  },
 }
